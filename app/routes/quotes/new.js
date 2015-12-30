b6e747e41c0a12b210f2d1807c31d4a3de50527d
talum <tracidini@gmail.com>
2015-12-29 19:37:42 -0500

    Create quote and associate with character dropdown

--- app/routes/quotes/new.js
+++ app/routes/quotes/new.js
@@ -2,11 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.createRecord('quote');
+    // return this.store.createRecord('quote');
+    return Ember.RSVP.hash({
+      quote: this.store.createRecord('quote'),
+      characters: this.store.findAll('character')
+    });
   }, 
+  setupController(controller, models){
+    controller.set('quote', models.quote);
+    controller.set('characters', models.characters);
+  },
   actions:{
-    save: function(model){
-      model.save().then((quote) => {
+    save: function(quote){
+      quote.save().then((quote) => {
         this.transitionTo('quotes.index');
       });
     }, 
