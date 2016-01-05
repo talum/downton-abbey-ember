9cc94a2ab8e936fa0dba49580786b61a37790f39
talum <tracidini@gmail.com>
2016-01-04 21:13:55 -0500

    Add image fields to form and component

--- app/models/character.js
+++ app/models/character.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   title: DS.attr('string'),
   socialClass: DS.attr('string'),
+  imageURL: DS.attr('string'),
   quotes: DS.hasMany('quote', {async: true}),
   actor: DS.belongsTo('actor', {async: true})
 });
