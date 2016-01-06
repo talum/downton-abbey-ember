ed2ad887bd2010b1623c786a5b682c6ff841d62c
talum <tracidini@gmail.com>
2016-01-06 11:47:16 -0500

    Add image to episodes

--- app/models/episode.js
+++ app/models/episode.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   summary: DS.attr('string'),
-  season: DS.belongsTo('season', {async: true})
-  
+  season: DS.belongsTo('season', {async: true}),
+  imageURL: DS.attr('string')
 });
