82a6b8cce87915b3c7242a224007bf70c58ae87d
talum <tracidini@gmail.com>
2016-01-07 17:07:18 -0500

    Add custom mobile-friendly menu

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,7 +19,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import("bower_components/bootstrap/dist/css/bootstrap.css");
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  // app.import("bower_components/bootstrap/dist/css/bootstrap.css");
+  // app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/jquery/dist/jquery.js');
+  app.import('bower_components/custom.js');
   return app.toTree();
 };
