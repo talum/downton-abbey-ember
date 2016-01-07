14160eda2d9fe6e56861f7f6c71aa64e5d63f5a1
talum <tracidini@gmail.com>
2016-01-06 19:16:47 -0500

    Add context to quotes and modify form

--- app/models/quote.js
+++ app/models/quote.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr('string'),
+  context: DS.attr('string'),
   character: DS.belongsTo('character', {async: true}),
   episode: DS.belongsTo('episode', {async: true})
 });
