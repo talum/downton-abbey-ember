d4a97c7212b9c5816054234727b87fb6e2dfed09
talum <tracidini@gmail.com>
2016-01-06 19:31:58 -0500

    Add character/episode links

--- app/models/episode.js
+++ app/models/episode.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   summary: DS.attr('string'),
   season: DS.belongsTo('season', {async: true}),
-  imageURL: DS.attr('string')
+  imageURL: DS.attr('string'),
+  quotes: DS.hasMany('quote', {async: true})
 });
