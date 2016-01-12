3c9432309d18aeb74c985136fedff2ad4fee6ebf
talum <tracidini@gmail.com>
2016-01-12 11:55:22 -0500

    Add season select menu on mobile

--- app/controllers/seasons.js
+++ app/controllers/seasons.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortProperties: ['name:asc'],
-  sortedModel: Ember.computed.sort('model', 'sortProperties')
+  sortedModel: Ember.computed.sort('model', 'sortProperties'), 
+  season: null,
+  actions:{
+    selectSeason(seasonParams){
+      let season = this.store.findRecord('season', seasonParams);
+      this.transitionToRoute("seasons.season", season);
+    }
+  }
 });
