c4efc50a57f1c4848ee4bade7b72b4817227d290
talum <tracidini@gmail.com>
2016-01-15 19:26:39 -0500

    Add login

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  host: "http://localhost:3000",
-  namespace: 'api/v1'
+  host: "http://localhost:3000"
+  // namespace: 'api/v1'
 });
