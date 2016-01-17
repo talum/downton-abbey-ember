8326be69d899275f409fbe340088358de3a247cf
talum <tracidini@gmail.com>
2016-01-17 10:14:27 -0500

    Add authentication to show-episode, show-season

--- app/components/show-episode.js
+++ app/components/show-episode.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
   doubleClick() {
+    if (this.get('session').get('isAuthenticated')){
       this.toggleProperty('isEditing');
+    }
     },
   actions: {
     update() {
